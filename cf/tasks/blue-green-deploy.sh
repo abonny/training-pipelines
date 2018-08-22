86f6a35a6edf40d667120cae9c76897dcd4a56f7
Luke Pahler <luke.pahler@cgi.com>
2018-08-22 15:16:03 -0600

    Get ready for cleanup.

--- cf/tasks/blue-green-deploy.sh
+++ cf/tasks/blue-green-deploy.sh
@@ -40,13 +40,15 @@ apps_url=`cf curl $routes_url | jq -r '.resources[].entity | select(.host=="'"$C
 
 echo "apps_url: shows the apps assigned to the hostname: ${apps_url}"
 
-# TO DO: Finish the jq parsing
 # Fetch the app names assigned to the hostname
-app_names=`(cf curl $apps_url | jq -r '...')`
+app_names=`(cf curl $apps_url | jq -r '.resources[].entity.name')`
 
 for name in $app_names; do
     if [ "$name" != "$app_name" ]
     then
       # TO DO: clean up blue
+      echo NOT CLEANING ON PURPOSE
+      #cf unmap-route $name $CF_DOMAIN --hostname $CF_HOSTNAME
+      #cf d -f -r $name
     fi
 done
